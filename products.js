35606a0033455063cef052794bab83115415161c
nearni <79463294+nearni@users.noreply.github.com>
2025-06-02 23:48:46 +0800

    Create products.js

---
+++ products.js
@@ -0,0 +1,7 @@
+const products = [
+  { id: 1, name: "狗狗飼料（大包裝）", price: 1200, image: "dog-food.jpg" },
+  { id: 2, name: "貓砂（10L）", price: 400, image: "cat-litter.jpg" },
+  { id: 3, name: "牙膏牙刷組", price: 150, image: "toothbrush.jpg" },
+  { id: 4, name: "衛生紙箱裝", price: 360, image: "tissue-box.jpg" },
+  { id: 5, name: "清潔噴霧（環保）", price: 220, image: "clean-spray.jpg" }
+];
